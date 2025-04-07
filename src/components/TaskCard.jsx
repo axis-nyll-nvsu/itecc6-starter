75e94be89a34c12775e4a5cbe8bb75285630d939
Vernyll Jan P. Asis <asisvjp.nvsu@gmail.com>
2025-04-07 11:16:31 +0800

    [feat] add adding and editing forms.

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,13 +1,13 @@
 import { LuPencilLine, LuSquareCheckBig, LuTrash } from 'react-icons/lu';
 
-export default function TaskCard({ task }) {
+export default function TaskCard({ task, editAction, deleteAction }) {
     return (
         <article className="flex items-center p-4 rounded border-[rgba(0,0,0,0.1)] border-1 shadow-xs">
             <p>{task.desc}</p>
             <div className="ml-auto flex gap-0.5 bg-stone-50 text-stone-500 inset-shadow-2xs p-1 text-center rounded">
                 <button className={`p-1 transition-colors hover:bg-green-600 ${task.done && "bg-green-600 text-white"} hover:text-white rounded cursor-pointer`}><LuSquareCheckBig /></button>
-                <button className="p-1 transition-colors hover:bg-amber-600 hover:text-white rounded cursor-pointer"><LuPencilLine /></button>
-                <button className="p-1 transition-colors hover:bg-red-600 hover:text-white rounded cursor-pointer"><LuTrash /></button>
+                <button className="p-1 transition-colors hover:bg-amber-600 hover:text-white rounded cursor-pointer" onClick={() => editAction(task.id)}><LuPencilLine /></button>
+                <button className="p-1 transition-colors hover:bg-red-600 hover:text-white rounded cursor-pointer" onClick={() => deleteAction(task.id)}><LuTrash /></button>
             </div>
         </article>
     );
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,32 +1,101 @@
 import { useEffect, useState } from 'react';
-import { LuChevronRight, LuPlus } from 'react-icons/lu';
+import {LuChevronRight, LuPlus, LuX} from 'react-icons/lu';
 import TaskCard from '../components/TaskCard';
-
 const mockTasks = [
     { id: 1, desc: "Play Video Game", done: false },
     { id: 2, desc: "Read Manga", done: true },
     { id: 3, desc: "Watch TikTok", done: true },
 ];
 
-export default function Tasks({searchKey}) {
-    const [tasks, setTasks] = useState(mockTasks);
+function AddTaskForm({submitAction, cancelAction}) {
     const [description, setDescription] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        submitAction({desc: description});
+        setDescription("");
+    }
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        setDescription("");
+        cancelAction();
+    }
+
+    return (
+        <div className="border-1 border-blue-800 rounded p-2 mb-4 ">
+            <h1 className="text-blue-800 font-semibold">Add Task</h1>
+            <form action="" className="flex justify-between">
+                <input type="text" required placeholder="What do you want to do?" value={description} maxLength={30}
+                       onChange={(e) => setDescription(e.target.value)} className="focus:outline-none w-full mr-2"/>
+                <button onClick={handleSubmit} className="bg-blue-800 text-white p-2 rounded cursor-pointer mr-1"><LuPlus/>
+                </button>
+                <button onClick={handleCancel} className="bg-blue-800 text-white p-2 rounded cursor-pointer"><LuX/>
+                </button>
+            </form>
+        </div>
+    );
+}
+
+function EditTaskForm({task, submitAction, cancelAction}) {
+    const [description, setDescription] = useState(task.description);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        task.description = description;
+        submitAction(task);
+        setDescription("");
+    }
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        setDescription("");
+        cancelAction();
+    }
+
+    return (
+        <div className="border-1 border-blue-800 rounded p-2 mb-4 ">
+            <h1 className="text-blue-800 font-semibold">Edit Task</h1>
+            <form action="" className="flex justify-between">
+                <input type="text" required placeholder="What do you want to do?" value={description} maxLength={30}
+                       onChange={(e) => setDescription(e.target.value)} className="focus:outline-none w-full mr-2"/>
+                <button onClick={handleSubmit} className="bg-blue-800 text-white p-2 rounded cursor-pointer mr-1"><LuPlus/>
+                </button>
+                <button onClick={handleCancel} className="bg-blue-800 text-white p-2 rounded cursor-pointer"><LuX/>
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default function Tasks({searchKey}) {
+    const [task, setTask] = useState({});
+    const [tasks, setTasks] = useState([]);
+    const [editing, setEditing] = useState(false);
 
     useEffect(() => {
         setTasks(mockTasks.filter((item) => item.desc.toLowerCase().includes(searchKey.toLowerCase())));
     }, [searchKey]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(description.trim() !== "") {
-            setTasks([...tasks, {
-                id: tasks.reduce((max, task) => (task.id > max ? task.id : max), 1),
-                desc: description.trim(),
-                done: false
-            }])
-        }
-        setDescription("");
+    const addTask = (t) => {
+        t.id = tasks.reduce((max, task) => (task.id > max ? task.id : max), 0) + 1;
+        t.done = false;
+        setTasks([...tasks, t]);
     }
+    const updateTask = (t) => {
+        setTasks([tasks.filter((item) => item.id !== t.id), t]);
+        setEditing(false);
+        setTask({});
+    }
+    const editTask = (id) => {
+        // TODO: Create an edit function.
+        setEditing(true);
+    }
+    const cancelTask = () => {
+        setTask({});
+        setEditing(false);
+    }
+    const deleteTask = (id) => setTasks(tasks.filter((item) => item.id !== id));
 
     return (
         <div className="mt-2 p-2 md:p-4">
@@ -38,17 +107,14 @@ export default function Tasks({searchKey}) {
                     <span>Tasks</span>
                 </div>
             </div>
-            <div className="border-1 border-blue-800 rounded p-2 mb-4 ">
-                <form action="" className="flex justify-between">
-                    <input type="text" placeholder="What do you want to do?" value={description} maxLength={30} onChange={(e) => setDescription(e.target.value)} className="focus:outline-none w-full mr-2" />
-                    <button onClick={handleSubmit} className="bg-blue-800 text-white p-2 rounded cursor-pointer"><LuPlus /></button>
-                </form>
-            </div>
+            {!editing ? <AddTaskForm submitAction={addTask} cancelAction={cancelTask} /> : <EditTaskForm task={task} submitAction={updateTask} cancelAction={cancelTask} />}
             <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {
                 tasks.length !== 0 ?
-                tasks.map(task => <TaskCard task={task} key={task.id} />) :
-                <div className="text-sm">No tasks found for "{searchKey}".</div>
+                tasks.map(task => <TaskCard task={task} key={task.id} editAction={editTask} deleteAction={deleteTask} />) :
+                searchKey ?
+                    <div className="text-sm">No tasks found for "{searchKey}".</div> :
+                    <div className="text-sm">No tasks yet.</div>
             }
             </div>
         </div>
